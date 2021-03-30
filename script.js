c03775f0b11fde2e18332c4063df46f61981c892
Daisha Marrow <marrowdaisha@gmail.com>
2021-03-30 19:15:37 -0400

    added to js

--- script.js
+++ script.js
@@ -1,54 +1,71 @@
-<<<<<<< HEAD
-//google.charts.load("current", {packages:["corechart"]});
-//google.charts.setOnLoadCallback(drawChart);
-//function drawChart() {
- // var data = google.visualization.arrayToDataTable([
-  //  ['Task', 'Hours per Day'],
-   // ['Work',     11],
-   // ['Eat',      2],
-   // ['Commute',  2],
-   // ['Watch TV', 2],
-   // ['Sleep',    7]
-  //]);
-
-  //var options = {
-   // title: 'My Wallet',
-    //pieHole: 0.4,
-  //};
-
- // var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
-  //chart.draw(data, options);
-//}
-
-function addMoney(){
+//declared variable, parseInt function converts its first argument to a string, parses that string, then returns an integer or NaN .
+      // Creates Table for new data
+let table = document.querySelector("table");
 let monthlyIncome = parseInt(document.getElementById("monthlyIncome").innerHTML);
- let currentIncome = parseInt(document.getElementById("currentIncome").innerHTML);
-    let money = parseInt(document.getElementById('monthlyIncome').value);
-    if (money == " " || isNaN(money)) {
-        alert("Please enter a valid amount")
+let currentIncome = parseInt(document.getElementById("currentIncome").innerHTML);  
+function addMoney(){
+  
+    //added if statement and alert if no value is added to monthly income
+        let money = parseInt(document.getElementById('money').value);
+                console.log(money);   
+        if (money == " " || isNaN(money)) {
+            alert("Please enter a valid amount");
+        }
+        //added value to a variable and assigned the results
+        else{
+            monthlyIncome += money;
+            currentIncome += money;
+            //used to return the element that has the ID attribute with the specified value
+            document.getElementById("monthlyIncome").innerHTML = monthlyIncome;
+            document.getElementById("currentIncome").innerHTML = currentIncome;
+        }
     }
-}
-
-
-=======
-      google.charts.load("current", {packages:["corechart"]});
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Task', 'Hours per Day'],
-          ['Work',     11],
-          ['Eat',      2],
-          ['Commute',  2],
-          ['Watch TV', 2],
-          ['Sleep',    7]
-        ]);
-
-        var options = {
-          title: 'My Wallet',
-          pieHole: 0.4,
-        };
-
-        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
-        chart.draw(data, options);
+    
+    function addExpense() {
+    let currentIncome = parseInt(document.getElementById("currentIncome").innerHTML);
+    let expenseName = document.getElementById('expenseName').value;
+    //
+      let expenseAmount = parseInt (document.getElementById('expenseAmount').value);
+      if (expenseName.length =="") {
+        alert("Enter your expense name");
       }
->>>>>>> e202a7e760c29cfabf653ec04c0eb8512322c50e
+      else if (expenseAmount == " " || isNaN(expenseAmount) ) {
+        alert('Enter your valid expense amount');
+      }
+      //if less than or equal to
+      else if (expenseAmount <= currentIncome){
+          //subtraction
+        currentIncome -= expenseAmount;
+        document.getElementById('currentIncome').innerHTML = currentIncome;
+      }
+      console.log(expenseName);
+      addRow (expenseName, expenseAmount);
+    }
+      function addRow(expenseName, expenseAmount) {
+        
+          let row = table.insertRow();
+            let cell = row.insertCell();
+            let text = document.createTextNode(expenseName);
+            cell.appendChild(text);
+            cell1 = row.insertCell();
+            let text1 = document.createTextNode(expenseAmount);
+            cell1.appendChild(text1);
+          }
+          
+          localStorage.setItem("expenseName", expenseName);
+          console.log(localStorage);
+
+
+          function init() {
+            // local storage
+            let table = JSON.parse(localStorage.getItem("addExpense"));
+            if (storedCities) {
+                cities = storedCities;
+                renderCities();
+                weather(cities[cities.length -1]);
+            }; 
+        };
+        init();
+
+   
+
