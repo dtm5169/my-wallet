d9ffeaf1519f0e7460a9a688300d46b02327bb80
artharvey <artharvey07@gmail.com>
2021-03-30 20:12:16 -0400

    accepting changes

--- script.js
+++ script.js
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-//google.charts.load("current", {packages:["corechart"]});
-//google.charts.setOnLoadCallback(drawChart);
-//function drawChart() {
- // var data = google.visualization.arrayToDataTable([
-  //  ['Task', 'Hours per Day'],
-   // ['Work',     11],
-   // ['Eat',      2],
-   // ['Commute',  2],
-   // ['Watch TV', 2],
-   // ['Sleep',    7]
-  //]);
-
-  //var options = {
-   // title: 'My Wallet',
-    //pieHole: 0.4,
-  //};
-
- // var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
-  //chart.draw(data, options);
-//}
-
-function addMoney(){
-let monthlyIncome = parseInt(document.getElementById("monthlyIncome").innerHTML);
- let currentIncome = parseInt(document.getElementById("currentIncome").innerHTML);
-    let money = parseInt(document.getElementById('monthlyIncome').value);
-    if (money == " " || isNaN(money)) {
-        alert("Please enter a valid amount")
-    }
-}
-
-
-=======
       google.charts.load("current", {packages:["corechart"]});
       google.charts.setOnLoadCallback(drawChart);
       function drawChart() {
@@ -51,4 +18,3 @@ let monthlyIncome = parseInt(document.getElementById("monthlyIncome").innerHTML)
         var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
         chart.draw(data, options);
       }
->>>>>>> e202a7e760c29cfabf653ec04c0eb8512322c50e
